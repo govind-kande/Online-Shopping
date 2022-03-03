ee4da4eb56ff1bcd893c10753572d8d7d84f47ca
Roshni Patil <roshni@2694@gmail.com>
2022-03-03 14:15:48 +0530

    Shopping Cart quantity increment decrement

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -11,5 +11,49 @@ export class ShoppingCartComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  orderTotal!: number;
+  productArray: any[] = [
+    {
+      img: "assets/images/55.png",
+      prodName: "Wrong",
+      prodId: "P5125",
+      price:500,
+      qnt:1,
+      totalAmt:500,
+    },
+    {
+      img: "assets/images/56.png",
+      prodName: "FUURAK",
+      prodId: "P5126",
+      price:1500,
+      qnt:1,
+      totalAmt:1500,
+    },
+    {
+      img: "assets/images/57.png",
+      prodName: "HRX",
+      prodId: "P5127",
+      price:800,
+      qnt:1,
+      totalAmt:800,
+    
+    }
+  ]
+
+  inc(prod: any){
+    if(prod.qnt != 5){
+      prod.qnt +=1;
+      prod.totalAmt = prod.qnt*prod.price;
+    }
+    
+  }
+
+  dec(prod: any){
+    if(prod.qnt != 1){
+      prod.qnt -=1;
+      prod.totalAmt = prod.qnt*prod.price;
+    }
+    
+  }
 
 }
